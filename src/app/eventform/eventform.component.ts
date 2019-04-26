948f671ce8889979d21794cb9f405f146751ad10
Shayan <sh.shmss@gmail.com>
2019-04-26 14:21:39 -0400

    events form connected to book titles

--- src/app/eventform/eventform.component.ts
+++ src/app/eventform/eventform.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core'; 
+import { Component, OnInit, Input } from '@angular/core';
 import { EventService } from '../event.service';
-import { Router} from '@angular/router'; 
-import { ActivatedRoute, ParamMap } from '@angular/router'; 
+import { Router} from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ClubService } from '../club.service';
+
 
 @Component({
   selector: 'app-eventform',
@@ -11,39 +13,50 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class EventformComponent implements OnInit {
 
   @Input() eventtitle: string;
-  @Input() eventurl: string; 
+  @Input() eventurl: string;
   @Input() street: string;
-  @Input() city: string; 
+  @Input() city: string;
   @Input() state: string;
-  @Input() zip: number; 
+  @Input() zip: number;
   @Input() country: string;
-  @Input() startdate: number; 
+  @Input() startdate: number;
   @Input() starttime: number;
-  @Input() enddate: number; 
+  @Input() enddate: number;
   @Input() endtime: number;
-  @Input() eventdescribe: string; 
+  @Input() eventdescribe: string;
   @Input() organizername: string;
-  
-  private mode = 'Add'; 
-  private id: string; 
 
-  
-  constructor(private _myService : EventService, private router:Router, public route: ActivatedRoute ) { }
+  private mode = 'Add';
+  private id: string;
+  public x;
+  public clubs;
+
+
+  constructor(private _myService : EventService, private _myService2: ClubService, private router:Router, public route: ActivatedRoute ) { }
 
   onSubmit(){
-    console.log("You submitted: " + this.eventtitle + " " + this.eventurl + " " + this.street + " " + this.city + " " + this.state + " " + this.zip + " " + this.country +  " " + this.startdate + " " + this.starttime + " " + this.enddate + " " + this.endtime + " " + this.eventdescribe + " " + this.organizername ); 
+    console.log("You submitted: " + this.eventtitle + " " + this.eventurl + " " + this.street + " " + this.city + " " + this.state + " " + this.zip + " " + this.country +  " " + this.startdate + " " + this.starttime + " " + this.enddate + " " + this.endtime + " " + this.eventdescribe + " " + this.organizername );
     if(this.mode == 'Add')
-      this._myService.addEvents(this.eventtitle, this.eventurl, this.street, this.city, this.state, this.zip, this.country, this.startdate, this.starttime, this.enddate, this.endtime, this.eventdescribe, this.organizername); 
+      this._myService.addEvents(this.eventtitle, this.eventurl, this.street, this.city, this.state, this.zip, this.country, this.startdate, this.starttime, this.enddate, this.endtime, this.eventdescribe, this.organizername);
     if(this.mode == 'Edit')
-      this._myService.updateEvent(this.id, this.eventtitle, this.eventurl, this.street, this.city, this.state, this.zip, this.country, this.startdate, this.starttime, this.enddate, this.endtime, this.eventdescribe, this.organizername); 
-    this.router.navigate(['/listEvent']);  
+      this._myService.updateEvent(this.id, this.eventtitle, this.eventurl, this.street, this.city, this.state, this.zip, this.country, this.startdate, this.starttime, this.enddate, this.endtime, this.eventdescribe, this.organizername);
+    this.router.navigate(['/listEvent']);
   }
-  
+
 
   ngOnInit() {
+    this.x = this._myService2.getClubs ();
+    this._myService2.getClubs().subscribe(
+        //read data and assign to public variable students
+        data => { this.clubs = data},
+        err => console.error(err),
+        () => console.log ('Clubs loaded')
+    );
+
+
     this.route.paramMap.subscribe((paramMap: ParamMap ) => {
       if (paramMap.has('_id'))
-        { this.mode = 'Edit'; /*request had a parameter _id */ 
+        { this.mode = 'Edit'; /*request had a parameter _id */
           this.id = paramMap.get('_id');}
       else {this.mode = 'Add';
           this.id = null; }
