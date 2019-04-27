fb4729614557d73853b049158021ad90e90dd73a
ksnik <49994570+ksnik@users.noreply.github.com>
2019-04-27 14:24:27 -0400

    Add files via upload

--- src/app/add-comment/add-comment.component.ts
+++ src/app/add-comment/add-comment.component.ts
@@ -16,24 +16,42 @@ export class AddCommentComponent implements OnInit {
 
   public x;
   public clubs;
-
+  public comments;
   private mode = 'add';
   private id: string;
   constructor(private _myService: CommentService, private _myService2: ClubService, private router: Router, public route: ActivatedRoute) { }
   onSubmit(){
  if(this.mode == 'add')
 this._myService.addComments(this.firstName ,this.lastName,this.bookname,this.comment);
-this.router.navigate(['/view']);
+window.location.replace('/view');
   console.log("You submitted: " + this.firstName + " " +
   this.lastName +" "+this.bookname+" "+this.comment);
   alert("Comments have been submitted. Thank you!");
-  this.router.navigate(['/view']);
+  window.location.replace('/view');
 location.reload();
 if(this.mode == 'edit')
 console.log("update called");
 this._myService.updateComments(this.id,this.firstName,this.lastName,this.bookname,this.comment);
+window.location.replace('/view');
   }
-  public comments;
+
+
+  myFunction2(x){
+    for (let i = 0; i < x.length; i++){
+         if(x[i]._id == this.id) {
+            return i;
+    }
+  }
+ }
+
+ myFunction3(x){
+  //  console.log (x)
+  this.firstName = x.firstName;
+  this.lastName = x.lastName;
+  this.bookname= x.bookname;
+
+ }
+
   ngOnInit(){
     this.x = this._myService2.getClubs ();
     this._myService2.getClubs().subscribe(
@@ -44,6 +62,9 @@ this._myService.updateComments(this.id,this.firstName,this.lastName,this.booknam
     );
 
 
+
+    
+
     this.route.paramMap.subscribe((paramMap: ParamMap ) => {
       if (paramMap.has('_id'))
       { this.mode = 'edit'; /*request had a parameter _id */
@@ -51,9 +72,25 @@ this._myService.updateComments(this.id,this.firstName,this.lastName,this.booknam
       else {this.mode = 'add';
       this.id = null; }
     });
-    this.getStudents();
+    this.getComments();
+
+    if (this.mode == 'edit') {
+      //
+      console.log('name came');
+       this.x = this._myService.getComments ();
+       this._myService.getComments().subscribe(
+            //read data and assign to public variable students
+            data => { this.comments = data},
+            err => console.error(err),
+            () => this.myFunction3(this.comments[this.myFunction2(this.comments)])
+           
+      );
+        // this.myFunction()
+      }
+
+
     }
-  getStudents() {
+    getComments() {
     this._myService.getComments().subscribe(
     //read data and assign to public variable students
     data => { this.comments = data},
